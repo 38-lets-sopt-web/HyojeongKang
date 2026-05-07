fd0c40678dea8dc9d928ec8509f5b004f0d0917a
ahyohyo <sywjsx0x@gmail.com>
2026-05-08 05:39:58 +0900

    feat: 회원 조회 페이지 API 연동

--- Week4/Homework/my-app/src/components/member-detail/MemberDetailCard.tsx
+++ Week4/Homework/my-app/src/components/member-detail/MemberDetailCard.tsx
@@ -1,33 +1,43 @@
 /** @jsxImportSource @emotion/react */
 import * as s from "./memberDetailCard.style";
 
-const MemberDetailCard = () => {
+interface MemberDetailCardProps {
+    data: {
+        loginId: string;
+        name: string;
+        email: string;
+        age: number;
+        part: string;
+    };
+}
+
+const MemberDetailCard = ({ data }: MemberDetailCardProps) => {
     return (
         <article css={s.infoCardWrapper}>
             <section css={s.infoBoxStyle}>
                 <div css={s.infoRowStyle}>
                     <span css={s.infoLabelStyle}>이름</span>
-                    <span css={s.infoValueStyle}>갱갱갱</span>
+                    <span css={s.infoValueStyle}>{data.name}</span>
                 </div>
 
                 <div css={s.infoRowStyle}>
                     <span css={s.infoLabelStyle}>아이디</span>
-                    <span css={s.infoValueStyle}>유저아이디</span>
+                    <span css={s.infoValueStyle}>{data.loginId}</span>
                 </div>
 
                 <div css={s.infoRowStyle}>
                     <span css={s.infoLabelStyle}>이메일</span>
-                    <span css={s.infoValueStyle}>이메일@이메일</span>
+                    <span css={s.infoValueStyle}>{data.email}</span>
                 </div>
 
                 <div css={s.infoRowStyle}>
                     <span css={s.infoLabelStyle}>나이</span>
-                    <span css={s.infoValueStyle}>22</span>
+                    <span css={s.infoValueStyle}>{data.age}</span>
                 </div>
 
                 <div css={s.infoRowStyle}>
                     <span css={s.infoLabelStyle}>파트</span>
-                    <span css={s.infoValueStyle}>웹</span>
+                    <span css={s.infoValueStyle}>{data.part}</span>
                 </div>
             </section>
         </article>
--- Week4/Homework/my-app/src/pages/search-member/SearchMember.tsx
+++ Week4/Homework/my-app/src/pages/search-member/SearchMember.tsx
@@ -4,8 +4,28 @@ import Input from "../../components/input/Input.tsx";
 import Button from "../../components/buttton/Button.tsx";
 import MemberDetailCard from "../../components/member-detail/MemberDetailCard.tsx";
 import Header from "../../components/header/Header.tsx";
+import axios from "axios";
+import { useState } from "react";
 
 const SearchMember = () => {
+    const [searchId, setSearchId] = useState('');
+    const [memberData, setMemberData] = useState(null);
+    const [searched, setSearched] = useState(false); 
+
+    const handleSearch = async (e: React.FormEvent) => {
+        e.preventDefault();
+
+        try {
+            const response = await axios.get(`${import.meta.env.VITE_API_URL}/users/${searchId}`);
+            setMemberData(response.data.data);
+            setSearched(true);
+
+        } catch (error) {
+            console.error('유저 정보 조회 실패', error);
+            setMemberData(null);
+            setSearched(true);
+        }
+    }
 
 
     return (
@@ -13,16 +33,17 @@ const SearchMember = () => {
             <Header />
             <section css={s.containerStyle}>
                 <h1>회원 조회</h1>
-                <form css={s.formStyle}>
+                <form css={s.formStyle} onSubmit={handleSearch}>
                     <Input
                         type="number"
                         placeholder="회원 ID를 입력하세요"
-                        label="회원 ID" />
+                        label="회원 ID"
+                        value={searchId}
+                        onChange={(e) => setSearchId(e.target.value)} />
                     <Button type="submit" variant='primary'>검색</Button>
                 </form>
                 <h3 css={s.resultTitleStyle}>검색 결과</h3>
-                <div css={s.emptyStyle}>원하는 ID를 검색해보세요</div>
-                {/* {!searched && (
+                {!searched && (
                     <div css={s.emptyStyle}>원하는 ID를 검색해보세요</div>
                 )}
                 {searched && !memberData && (
@@ -30,7 +51,7 @@ const SearchMember = () => {
                 )}
                 {searched && memberData && (
                     <MemberDetailCard data={memberData} />
-                )} */}
+                )}
             </section>
         </>
     );
